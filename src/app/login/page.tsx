bd04ff925c62f9a39fc11b95be82027a2210e3af
pillow12360 <ehdcks1256@naver.com>
2025-06-02 13:56:41 +0900

    CHAM-34 feat: 로그인 페이지 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
 
     useEffect(() => {
         if (isAuthenticated) {
-            router.push('/dashboard')
+            router.push('/')
         }
     }, [isAuthenticated, router])
 
