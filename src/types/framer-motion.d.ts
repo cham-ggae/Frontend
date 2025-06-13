37ce9d46f7b69766e44e329aa89274637d53e226
pillow12360 <ehdcks1256@naver.com>
2025-06-13 09:48:13 +0900

    CHAM-58 type: framer-motion.d.ts 전역 타입 단언 추가

---
+++ src/types/framer-motion.d.ts
@@ -0,0 +1,13 @@
+declare module 'framer-motion' {
+    export interface Variants {
+        [key: string]: any
+    }
+
+    export interface Transition {
+        [key: string]: any
+    }
+
+    export interface MotionProps {
+        [key: string]: any
+    }
+}
