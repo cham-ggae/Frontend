1417ef9ab8a0dfac72dce4ab213fd99a217b447a
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-06-16 11:27:17 +0900

    CHAM-59 FIX: welcome prompt 변경

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -30,8 +30,8 @@ export default function ChatMessage({
       id: 'welcome',
       content:
         isFamilyMode && familyMembers.length > 0
-          ? `안녕하세요! 가족 맞춤형 요금제 추천 챗봇입니다. 현재 ${familyMembers.length}명 가족 정보를 바탕으로 도와드릴게요! 💕`
-          : '안녕하세요! 개인 맞춤형 요금제 추천 챗봇입니다. 무엇이든 물어보세요!',
+          ? `안녕하세요! 가족 맞춤형 요금제 추천 챗봇 MODi입니다. 현재 ${familyMembers.length}명 가족 정보를 바탕으로 도와드릴게요! 💕`
+          : '"안녕하세요! 개인 맞춤형 요금제 추천을 위한 MODi 챗봇입니다. 당신의 통신 상황에 맞는 최적의 요금제를 찾아드릴게요!"',
       isUser: false,
       timestamp: new Date(),
     },
